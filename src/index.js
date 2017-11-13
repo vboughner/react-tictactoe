85e85958637ecf9bb731f2a27a754371f6d342e8
Van Boughner <van.boughner@ntti3.com>
2017-11-13 10:28:24 -0800

    Re-implemented the board to use row and column loops

--- src/index.js
+++ src/index.js
@@ -14,30 +14,35 @@ class Board extends React.Component {
     renderSquare(i) {
         return (
             <Square
+                key={i}
                 value={this.props.squares[i]}
                 onClick={() => this.props.onClick(i)}
             />
         );
     }
 
+    renderRow(r) {
+        let columns = [];
+        for (let c = 0; c < 3; c++) {
+            columns.push(this.renderSquare((r * 3) + c));
+        }
+
+        return (
+            <div key={r} className="board-row">
+                {columns}
+            </div>
+        );
+    }
+
     render() {
+        let rows = [];
+        for (let r = 0; r < 3; r++) {
+            rows.push(this.renderRow(r));
+        }
+
         return (
             <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+                {rows}
             </div>
         );
     }
